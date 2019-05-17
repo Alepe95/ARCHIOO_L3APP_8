5cb6d41b03a404f575770d5bb5d6f472b79e559b
Perez Alexis <perez_alexis@hotmail.fr>
2019-05-17 18:15:40 +0200

    Ajout d'exception plus mise en forme des packages

--- premierCoursCalculatrice/src/premierCoursCalculatrice/Calculator.java
+++ premierCoursCalculatrice/src/premierCoursCalculatrice/Calculator.java
@@ -1,10 +1,12 @@
 package premierCoursCalculatrice;
 
+import exception.MonException;
+
 public class Calculator {
 
 	private static Operation op;
 	
-	public static int execute(int a, int b,char c) {
+	public static int execute(int a, int b,char c) throws MonException {
 		op = CalculatorConf.getOperator(c);
 		return op.execute(a, b);
 	}
